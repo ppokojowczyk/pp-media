9bef65266fad532df89e70c29587f099a5e282bb
Piotr Pokojowczyk <piotrpokojowczyk@gmail.com>
2024-06-23 23:25:38 +0200

    Ability to show data in JSON on edit form.

--- frontend/src/components/edit.js
+++ frontend/src/components/edit.js
@@ -6,6 +6,8 @@ const Edit = ({ fields = [], save = null, cancel = null, data: d = {} }) => {
 
     const [data, setData] = useState(d);
 
+    const [jsonVisible, setJsonVisible] = useState(false);
+
     const handleFieldUpdate = (field, newValue) => {
         const newData = { ...data };
         newData[field] = newValue;
@@ -46,7 +48,18 @@ const Edit = ({ fields = [], save = null, cancel = null, data: d = {} }) => {
                         height: 0,
                         border: 'none'
                     }} />
+                    {
+                        jsonVisible && <div>
+                            <pre>
+                                {JSON.stringify(data, null, 2)}
+                            </pre>
+                        </div>
+                    }
                     <div className="buttons">
+                        <Button text="JSON" onClick={(e) => {
+                            e.preventDefault();
+                            setJsonVisible(!jsonVisible);
+                        }}></Button>
                         <Button text="Save" type="submit" />
                         <Button text="Cancel" onClick={cancel} />
                     </div>
