f514ab98c93bc2a513a67418f954e9b97190f727
Piotr Pokojowczyk <piotrpokojowczyk@gmail.com>
2024-07-03 23:27:42 +0200

    media types file

---
+++ frontend/src/utils/media-types.js
@@ -0,0 +1,16 @@
+const mediaTypes = [
+    {
+        type: 'movies',
+    },
+    {
+        type: 'games',
+    },
+    {
+        type: 'books',
+    },
+    {
+        type: 'albums',
+    },
+];
+
+export default mediaTypes;
