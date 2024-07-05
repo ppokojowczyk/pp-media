896f41c57f624a835b54703457b6d62803d7ca18
Piotr Pokojowczyk <piotrpokojowczyk@gmail.com>
2024-07-05 12:12:55 +0200

    add singular media types constants

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -3,6 +3,21 @@ const MOVIES_TYPE = "movies";
 const GAMES_TYPE = "games";
 const BOOKS_TYPE = "books";
 const ALBUMS_TYPE = "albums";
+const MOVIE_TYPE = 'movie';
+const GAME_TYPE = 'game';
+const BOOK_TYPE = 'book';
+const MUSIC_TYPE = 'music';
 const API_URL = process.env.REACT_APP_API_URL;
 
-export { APP_TITLE, MOVIES_TYPE, GAMES_TYPE, BOOKS_TYPE, ALBUMS_TYPE, API_URL };
+export {
+    APP_TITLE,
+    MOVIES_TYPE,
+    GAMES_TYPE,
+    BOOKS_TYPE,
+    ALBUMS_TYPE,
+    MOVIE_TYPE,
+    GAME_TYPE,
+    BOOK_TYPE,
+    MUSIC_TYPE,
+    API_URL,
+};
