89f90ea7f5a562e190a00d6912d1dc2fc8ab9595
Piotr Pokojowczyk <piotrpokojowczyk@gmail.com>
2024-07-07 11:48:15 +0200

    refresh button for statistics

--- frontend/src/components/statistics.js
+++ frontend/src/components/statistics.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import { API_URL } from "../utils/constants";
+import Button from "./button";
+import Header from "./header";
 
 const Statistics = () => {
 
@@ -13,24 +15,22 @@ const Statistics = () => {
                 .then(({ data }) => {
                     return data;
                 });
-        }
+        },
     };
 
-    useEffect(() => {
-        repository
-            .statistics()
-            .then((d) => {
-                setData(d);
-            })
-    }, []);
+    const load = () => repository.statistics().then((d) => setData(d));
+
+    useEffect(() => { load(); }, []);
 
     return (
         <div className="statistics">
-            {
-                data.map((d) => {
-                    return <div>{d.name} &rarr; <span>{d.value}</span></div>
-                })
-            }
+            <Header>Statistics</Header>
+            <Button
+                text="Refresh"
+                onClick={load}
+            />
+            <br /><br />
+            {data.map((d) => <div>{d.name} &rarr; <span>{d.value}</span></div>)}
         </div>
     )
 };
