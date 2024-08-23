a13c6273934e04358df240b3ec428898dba29bda
Piotr Pokojowczyk <piotrpokojowczyk@gmail.com>
2024-08-23 23:03:08 +0200

    function to get media type by media class

--- backend/src/Entity/MediaType.php
+++ backend/src/Entity/MediaType.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Exception;
+
 class MediaType
 {
     public static function movie(): string
@@ -22,5 +24,21 @@ class MediaType
     public static function album(): string
     {
         return "album";
+    }
+
+    public static function typeByMediaClass(string $mediaClass): string
+    {
+        switch ($mediaClass) {
+            case Album::class:
+                return static::album();
+            case Book::class:
+                return static::book();
+            case Movie::class:
+                return static::movie();
+            case Game::class:
+                return static::game();
+            default:
+                throw new Exception('Media type not found.');
+        }
     }
 }
