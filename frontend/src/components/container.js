b9e872c9caba8b7ba7b5a52b800c344621492db5
Piotr Pokojowczyk <piotrpokojowczyk@gmail.com>
2024-08-24 00:35:39 +0200

    container component

---
+++ frontend/src/components/container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Container = ({
+    className,
+    children,
+}) => {
+    return (
+        <div className={className}>{children}</div>
+    )
+};
+
+export default Container;
