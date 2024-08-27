6fd9e83cc335f5c14be6fd7b9729dc3dbb6995a2
Piotr Pokojowczyk <piotrpokojowczyk@gmail.com>
2024-08-27 23:53:25 +0200

    container props update

--- frontend/src/components/container.js
+++ frontend/src/components/container.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
 const Container = ({
+    id,
     className,
     children,
     onClick,
     style,
+    onDrop,
+    onDragOver,
 }) => {
     return (
         <div
+            id={id}
             className={className}
             onClick={onClick}
             style={style}
+            onDrop={onDrop}
+            onDragOver={onDragOver}
         >{children}</div>
     )
 };
