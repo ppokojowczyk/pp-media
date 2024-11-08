e8a063dfcfc36dc5d7428d1a9d82fd021946c311
Piotr Pokojowczyk <piotrpokojowczyk@gmail.com>
2024-11-08 23:41:14 +0100

    refactor MediaFilter

--- backend/src/Repository/MediaFilter.php
+++ backend/src/Repository/MediaFilter.php
@@ -6,54 +6,98 @@ use Symfony\Component\HttpFoundation\Request;
 
 class MediaFilter
 {
+    private const SORT_FILTER = 'sort';
+
+    private const ORDER_FILTER = 'order';
+
+    private const OWN_FILTER = 'own';
+
     /**
-     * @var array
+     * @var array $filters
      */
     private $filters;
 
-    private function processFiltersArray(array $filters)
+    /**
+     * Array with filters types.
+     * @return string[]
+     */
+    private static function types(): array
     {
-        $processed = [
-            'sort' => '',
-            'order' => '',
-            'own' => '',
+        return [
+            static::SORT_FILTER,
+            static::ORDER_FILTER,
+            static::OWN_FILTER,
         ];
+    }
 
-        foreach (['sort', 'order', 'own'] as $k) {
-            if (isset($filters[$k])) {
-                $processed[$k] = $filters[$k];
-            }
+    /**
+     * Process filters array, fill up values.
+     * @param array $filters
+     *
+     * @return array
+     */
+    private function processFiltersArray(array $filters): array
+    {
+        $processed = [];
+
+        foreach ($this->types() as $filter) {
+            $processed[$filter] = isset($filters[$filter])
+                ? $filters[$filter]
+                : '';
         }
 
         return $processed;
     }
 
+    /**
+     * @param array $filters
+     */
     public function __construct(array $filters)
     {
         $this->filters = $this->processFiltersArray($filters);
     }
 
+    /**
+     * Return value for `sort` filter.
+     * @return string
+     */
     public function sort(): string
     {
-        return $this->filters['sort'];
+        return $this->filters[static::SORT_FILTER];
     }
 
+    /**
+     * Return value for `order` filter.
+     * @return string
+     */
     public function order(): string
     {
-        return $this->filters['order'];
+        return $this->filters[static::ORDER_FILTER];
     }
 
+    /**
+     * Return value for `own` filter.
+     * @return string
+     */
     public function own(): string
     {
-        return $this->filters['own'];
+        return $this->filters[static::OWN_FILTER];
     }
 
+    /**
+     * Create a new MediaFilter instance from Request.
+     * @param Request $request
+     *
+     * @return self
+     */
     public static function fromRequest(Request $request): self
     {
-        return new static([
-            'sort' => $request->get('sort'),
-            'order' => $request->get('order'),
-            'own' => $request->get('own'),
-        ]);
+        $filters = [];
+
+        foreach (static::types() as $type) {
+            $filters[$type] = $request->get($type);
+        }
+
+        return new static($filters);
     }
 }
